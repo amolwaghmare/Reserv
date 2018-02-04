0c1f44cc7f0c0fc58f850678c2ba41091f5f45d6
Amol <Amol@sowmiyas-MacBook-Pro.local>
2018-02-04 21:39:25 +0000

    Updated logic to reseve family tickets - 3 adjacent.

--- src/main/java/Reservation.java
+++ src/main/java/Reservation.java
@@ -8,16 +8,37 @@ public class Reservation {
 		// String contains the reservation seats in a plane.
 		String bookedSeats = "67J 21E 25C 25A 33A 1K 54J 37C 37E 37A 14J 13A 40K 6D 2D 2K 4C 1A";
 
+		String[] seatsList = sortBookedList(bookedSeats);
+
+		String[][] seats = prepareSeats(seatsList);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservSeats(seats, 1,bookedSeats);
+		bookedSeats = reservFamilySeats(seats, 1,bookedSeats);
+		printSeats(seats);
+		System.out.println("Booked Seats: "+bookedSeats);
+	}
+
+	private static String[] sortBookedList(String bookedSeats) {
 		String[] seatsList = bookedSeats.split(" ");
 		String[] sortedBookedList = new String[seatsList.length];
 
 		for (int j = 0; j < seatsList.length; j++) {
-			System.out.println("" + seatsList[j]);
 			for (int i = 0; i < sortedBookedList.length; i++) {
 
 				if (compare(seatsList[j], seatsList[i]) > 0) {
 
-					System.out.println(seatsList[j] + " is more than " + seatsList[i]);
+					//System.out.println(seatsList[j] + " is more than " + seatsList[i]);
 					String temp = seatsList[i];
 					seatsList[i] = seatsList[j];
 					seatsList[j] = temp;
@@ -26,17 +47,7 @@ public class Reservation {
 				}
 			}
 		}
-
-		System.out.println("sortedBookedList:" + seatsList.toString());
-		for (int i = 0; i < seatsList.length; i++) {
-
-			System.out.println("" + seatsList[i]);
-
-		}
-		String[][] seats = prepareSeats(seatsList);
-		reservSeats(seats, "5D");
-		reservSeats(seats, "5E");
-		reservSeats(seats, "12E");
+		return seatsList;
 	}
 
 	public static int compare(String s1, String s2) {
@@ -69,48 +80,93 @@ public class Reservation {
 				String seat = "" + outter + "" + rowseat[inner];
 				if (doesContain(seat, seatsList)) {
 					// System.out.println("Seat reserved :"+seat);
-					System.out.print("[" + seat + "]");
+					//System.out.print("[" + seat + "]");
 
-					planeSeats[outter][inner] = "[" + seat + "]";
+					//planeSeats[outter][inner] = "[" + seat + "]";
+					planeSeats[outter][inner] = "Y";
 				} else {
 					seat = "";
 					// planeSeats[outter][inner] = "["+seat+"]";
 				}
 
 			}
-			System.out.println("");
+			//System.out.println("");
 		}
 		return planeSeats;
 
 	}
 
-	public static String[][] reservSeats(String[][] seats, String reserverSeat) {
+	public static String reservSeats(String[][] seats, int requestedSeats, String bookedSeats) {
 
 		boolean booked = false;
 		for (int outter = 1; outter < seats.length; outter++) {
 			if (!booked) {
 				for (int inner = 0; inner < seats[outter].length; inner++) {
 					String seat = seats[outter][inner];
-					System.out.println(" Seat::" + seat);
-
+					//System.out.println(" Seat::" + seat);
 					if (null != seat) {
 						System.out.print("[" + seat + "] is reserved. ");
 
 					} else {
-
-						System.out.print("[" + "" + outter + "" + rowseat[inner] + "] is free. ");
-						seats[outter][inner] = reserverSeat;
+						String seatNumber = "" + outter + "" + rowseat[inner] ;
+						System.out.print("[" + seatNumber+ "] is free. ");
+						seats[outter][inner] = "Y";
+						bookedSeats = bookedSeats + " "+seatNumber;
+						System.out.println("Booked Seats:" +bookedSeats);
 						booked = true;
 						break;
+					} 
+				}
 
+				//System.out.println("");
+			}
+		}
+
+		System.out.println("");
+		return bookedSeats;
+
+	}
+	public static String reservFamilySeats(String[][] seats, int requestedSeats, String bookedSeats) {
+
+		boolean booked = false;
+		for (int outter = 1; outter < seats.length; outter++) {
+		
+				if(seats[outter][0] == null && seats[outter][1] == null  && seats[outter][2] == null ) {
+					seats[outter][0] = "Y";
+					seats[outter][1] = "Y";
+					seats[outter][2] = "Y";
+					bookedSeats = " "+outter+"A"+" "+outter+"B"+" "+outter+"C"; 
+				}
+				if(seats[outter][7] == null && seats[outter][8] == null  && seats[outter][9] == null ) {
+					seats[outter][7] = "Y";
+					seats[outter][8] = "Y";
+					seats[outter][9] = "Y";
+					bookedSeats = " "+outter+"H"+" "+outter+"J"+" "+outter+"K";
+				}
+		}
+
+		System.out.println("");
+		return bookedSeats;
+
+	}
+	
+	public static void printSeats(String[][] seats) {
+
+		for (int outter = 1; outter < seats.length; outter++) {
+				for (int inner = 0; inner < seats[outter].length; inner++) {
+					String seat = seats[outter][inner];
+					if (null != seat) {
+						System.out.print( "["+seat+"]" );
+
+					} else {
+						System.out.print("[]");
+						
 					}
 				}
 
 				System.out.println("");
 			}
-		}
-		return planeSeats;
-
+		
 	}
 
 }
